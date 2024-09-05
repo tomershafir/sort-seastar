0946c3970afc54087a5e3eefacfc3d468b2bbf65
tomershafir <tomer.shafir8@gmail.com>
2024-09-05 15:04:39 +0300

    latency.sh: support child process sigint

--- test/perf/latency.sh
+++ test/perf/latency.sh
@@ -3,14 +3,26 @@
 [[ -z $SSORT_TESTDATA_PATH ]] && _SSORT_TESTDATA_PATH='test/testdata' || _SSORT_TESTDATA_PATH=$SSORT_TESTDATA_PATH
 [[ -z $SSORT_FLAGS ]] && _SSORT_FLAGS='' || _SSORT_FLAG=$SSORT_FLAGS
 
+SIG_TERM_EXIT_BASE=128
+SIGINT=2
+SIGINT_EXIT=$(($SIG_TERM_EXIT_BASE + $SIGINT))
+
+clean_setup() {
+    rm -rf $1 $2
+}
+
 for compressed in $_SSORT_TESTDATA_PATH/*.txt.gz; do
     decompressed=${compressed%".gz"} &&
     gunzip -c $compressed > $decompressed
-
+    actual=${decompressed}.sorted
     for i in {1..10}; do
         echo "$compressed $i:" &&
         time -p $_SSORT_BUILD_DIR/ssort $SSORT_FLAGS $decompressed 2>/dev/null
+        if [ $? -eq $SIGINT_EXIT ]; then
+            echo "[ERROR] latency.sh: ssort child process was interrupted"
+            clean_setup $decompressed $actual
+            exit $SIGINT_EXIT
+        fi
     done
-
-    rm -rf $decompressed
+    clean_setup $decompressed $actual
 done
